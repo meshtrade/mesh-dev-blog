d89a40b6ce857ad31416b2ccb66bf503154d03ad
KyleSmith19091 <skorpion19091@gmail.com>
2024-10-21 09:01:25 +0200

    static folder structure cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'AI';
-export const SITE_DESCRIPTION = 'Mesh.Trade AI';
+export const SITE_TITLE = 'Mesh.Dev';
+export const SITE_DESCRIPTION = 'Mesh.Trade Engineering Blog';
