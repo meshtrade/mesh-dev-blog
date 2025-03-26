15eb44592b38659f5b7a0dc42d8a0a363c39a0ce
KyleSmith19091 <skorpion19091@gmail.com>
2025-03-26 13:51:18 +0200

    add google bucket api constants

--- ui/src/consts.ts
+++ ui/src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'Mesh.Dev';
 export const SITE_DESCRIPTION = 'Mesh.Trade Engineering Blog';
+export const BUCKET_NAME = 'meshtrade-development-blog-audio'; // Replace with your public bucket name
+export const GOOGLE_API_URL = `https://storage.googleapis.com/storage/v1/b/meshtrade-development-blog-audio/o`;
